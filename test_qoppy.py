ad7a2c6ad1a1332c4290c8c745159586321fb1c4
Tobias Pape <tobias@netshed.de>
2013-01-11 08:55:16 +0100

    more meaningful tests

--- test_qoppy.py
+++ test_qoppy.py
@@ -1,10 +1,27 @@
 from qoppy.runtime import get_runtime
 from qoppy.parser import parse
-from qoppy.execution_model import w_nil, QuoppaException
+from qoppy.execution_model import *
 
 r = get_runtime()
 
 def test_primitives():
-    r.interpret(w_nil, parse("(1)"))
+    w_res = r.interpret(w_nil, parse("1")[0])
+    assert isinstance(w_res, W_Integer)
+
+    w_res = r.interpret(w_nil, parse("nil")[0])
+    assert w_res is w_nil
+
+    w_res = r.interpret(w_nil, parse('"foo"')[0])
+    assert isinstance(w_res, W_String)
+
+    w_res = r.interpret(w_nil, parse('barf')[0])
+    assert isinstance(w_res, W_Symbol)
+
+    w_res = r.interpret(w_nil, parse("1.1")[0])
+    assert isinstance(w_res, W_Real)
+
+    #    w_res = r.interpret(w_nil, parse("")[0])
+    #    assert isinstance(w_res, W_Integer)
+    
 
 
