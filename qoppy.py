61c6f4f63b41dcf2cca38579f2535a30d2d276c7
Tim Felgentreff <timfelgentreff@gmail.com>
2013-11-10 21:17:53 +0100

    executable name

--- qoppy.py
+++ qoppy.py
@@ -64,7 +64,8 @@ def entry_point(argv):
         print "Usage: %s [quoppa source file]" % argv[0]
         return 1
 
-def target(*args):
+def target(driver, *args):
+    driver.exe_name = 'qoppy-%(backend)s'
     return entry_point, None
 
 if __name__ == "__main__":
